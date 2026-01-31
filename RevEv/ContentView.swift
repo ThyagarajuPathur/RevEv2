ff0f95ea5d9c6918cd895896d593f259ba4c6104
ThyagarajuPathur <33411138+ThyagarajuPathur@users.noreply.github.com>
2026-01-31 22:17:23 +0530

    Initial Commit

---
+++ RevEv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RevEv
+//
+//  Created by Pathur Thyagaraju on 31/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RevEv/RevEvApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevEvApp.swift
+//  RevEv
+//
+//  Created by Pathur Thyagaraju on 31/01/26.
+//
+
+import SwiftUI
+
+@main
+struct RevEvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
